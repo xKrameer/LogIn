400fc92150399b57277ed426a65acaea4bbc9b31
SantiagoCañon <snti-2000@hotmail.com>
2024-04-12 17:27:26 -0500

    Cambio título

--- Front/index.php
+++ Front/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Iniciar Sesión</title>
+    <title>Proyecto LogIn</title>
 </head>
 <body>
     <h1>Iniciar Sesión</h1>
